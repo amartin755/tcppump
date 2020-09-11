f2b824016b41dab9c21cb2051fa6caff6d448888
amartin755 <netnag@mailbox.org>
2020-09-11 14:10:39 +0200

    added missing bugon.h

---
+++ src/libnetnag/bugon.h
@@ -0,0 +1,42 @@
+/**
+ * TCPPUMP <https://github.com/amartin755/tcppump>
+ * Copyright (C) 2012-2020 Andreas Martin (netnag@mailbox.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef BUGON_H
+#define BUGON_H
+
+#ifdef assert
+#error do not combine assert.h and bugon.h
+#endif
+
+#ifdef NDEBUG
+#define NDEBUG_WAS_DEFINED 1
+#endif
+
+#undef NDEBUG
+#include <assert.h>
+
+#define BUG_ON assert
+
+#ifdef NDEBUG_WAS_DEFINED
+#define NDEBUG 1
+#endif
+
+#undef NDEBUG_WAS_DEFINED
+
+#endif /* BUGON_H */
