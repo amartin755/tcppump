amartin755/tcppump

commit bc625e474824815e3d53c77ea38a3aa09990c94b

First step to kernel based packet synchronization on windows
This will lead to increased throughput and accuracy.
NOTE: Not even buildable on windows and linux!

src/os/linux/interface.cpp
@@ -83,6 +83,7 @@ bool cInterface::open ()
     return (ifcHandle != -1);
 }
 
+
 bool cInterface::close ()
 {
     // aleady closed
@@ -194,4 +195,10 @@ bool cInterface::getIPv4 (cIpAddress &ip)
         ip.set(myIP);
     }
     return true;
+}
+
+
+bool cInterface::isOpen () const
+{
+    return (ifcHandle != -1);
 }

src/os/linux/interface.hpp
@@ -35,9 +35,12 @@ public:
     virtual ~cInterface();
     bool open ();
     bool close ();
+    bool prepareSendQueue (int packetCnt, size_t totalBytes){return true;}
     bool sendPacket (const uint8_t* payload, size_t length) const;
+    bool flushSendQueue (void){return true;}
     bool getMAC (cMacAddress&);
     bool getIPv4 (cIpAddress&);
+    bool isOpen () const;
 
 private:
 

src/tcppump.cpp
@@ -260,6 +260,8 @@ int cTcpPump::execute (int argc, char* argv[])
         else
             Console::PrintMoreVerbose ("Repeating infinitely.\n\n", options.repeat);
 
+        ifc.prepareSendQueue(packets.size() * options.repeat, packets.size() * options.repeat * cEthernetPacket::MAX_DOUBLE_TAGGED_PACKET);
+
         auto t1 = std::chrono::high_resolution_clock::now();
         while (gSigIntStatus == 0 && (endless || options.repeat--))
         {
@@ -277,6 +279,7 @@ int cTcpPump::execute (int argc, char* argv[])
                 sentBytes += p.getLength ();
             }
         }
+        ifc.flushSendQueue();
         auto t2 = std::chrono::high_resolution_clock::now();
         auto elapsedUs = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
         seconds = (double)elapsedUs.count()/1000000.0;

src/os/CMakeLists.txt
@@ -33,6 +33,7 @@ set (SOURCES
      ${OS_SPECIFIC}/interface.cpp
      ${OS_SPECIFIC}/getch.cpp
      ${OS_SPECIFIC}/sleep.cpp
+     ${OS_SPECIFIC}/arp.cpp
      PARENT_SCOPE
 )
 set (INCLUDES

src/os/win32/interface.cpp
@@ -18,7 +18,9 @@
 
 
 #include <string>
+#include <thread>
 #include <pcap.h>
+#include <Win32-Extensions.h>
 #include <iphlpapi.h>
 #include <winerror.h>
 
@@ -26,9 +28,95 @@
 
 #include "bug.h"
 #include "console.hpp"
+#include "ethernetpacket.hpp"
+
+
+const int PCAP_Q_SIZE = 20*1024*1024;
+
+class cJob
+{
+    cQueue<pcap_send_queue*, 3> *pMsgQ;
+    pcap_send_queue *currSendQueue;
+    size_t bytesInCurrSendQueue;
+    std::thread *workerThread;
+    pcap_t *ifcHandle;
+    size_t bytesQueued;
+    size_t bytesSent;
+    int synchronized;
+
+public:
+    cJob (pcap_t *ifcHandle, int packetCnt, size_t totalBytes, int syn)
+    {
+        this->ifcHandle = ifcHandle;
+        pMsgQ = new cQueue<pcap_send_queue*, 3>();
+
+        const size_t wholeNeededMemory = packetCnt > 0 ? packetCnt * sizeof (pcap_pkthdr) + totalBytes : PCAP_Q_SIZE;
+
+        currSendQueue = pcap_sendqueue_alloc (wholeNeededMemory >= PCAP_Q_SIZE ? PCAP_Q_SIZE : wholeNeededMemory);
+        if (!currSendQueue)
+            throw std::bad_alloc();
+
+        bytesInCurrSendQueue = 0;
+        bytesSent            = 0;
+        bytesQueued          = 0;
+        synchronized         = sync;
+
+        workerThread = new std::thread (std::ref(*this));
+    }
+    bool addPacket (const uint8_t* payload, size_t length, const cTimeval& t)
+    {
+        if (!currSendQueue)
+        {
+            currSendQueue = pcap_sendqueue_alloc (PCAP_Q_SIZE);
+            bytesInCurrSendQueue = 0;
+        }
+        pcap_pkthdr hdr;
+        hdr.caplen = length;
+        hdr.len    = length;
+        hdr.ts     = t.timeval ();
+
+        BUG_ON (!pcap_sendqueue_queue (currSendQueue, &hdr, payload)); // can only fail if buffer calculation is wrong
+        bytesInCurrSendQueue += length + sizeof (pcap_pkthdr);
+        bytesQueued          += length + sizeof (pcap_pkthdr);
+
+        if (bytesInCurrSendQueue + cEthernetPacket::MAX_DOUBLE_TAGGED_PACKET + sizeof (pcap_pkthdr) >= PCAP_Q_SIZE) // don't know the size of next packet, assume maximum length
+        {
+            pMsgQ->push (currSendQueue);
+            currSendQueue = nullptr;
+        }
+
+        return true;
+    }
+    void operator ()()
+    {
+        pcap_send_queue *sendQueue;
+        while ((sendQueue = pMsgQ->pop ()))
+        {
+            bytesSent += pcap_sendqueue_transmit (ifcHandle, sendQueue, synchronized);
+            pcap_sendqueue_destroy (sendQueue);
+        }
+    }
+
+    ~cJob ()
+    {
+        if (currSendQueue) // is there something to send?
+        {
+            pMsgQ->push (currSendQueue);
+            currSendQueue = nullptr;
+        }
+        pMsgQ->push (nullptr); // send thread term signal
+        workerThread->join ();
+
+        delete pMsgQ;
+        delete workerThread;
+    }
+
+};
+
+
 
 /*
- * note: Because WINPCAPSs interface naming is weired, I try a more user friendly approach.
+ * note: Because WINPCAPSs interface naming is weird, I try a more user friendly approach.
  * On windows, ifname can either be the not changeable AdapterName (GUID), or the so-called FriendlyName, which is changeable by the user.
  * Both will be checked case-sensitive!
  * Examples: FriendlyName "WiFi" or "Local Area Connection 1."
@@ -37,8 +125,9 @@
 cInterface::cInterface(const char* ifname)
 : name (ifname)
 {
-    ifcHandle  = NULL;
-    winNetAdapters = NULL;
+    ifcHandle  = nullptr;
+    winNetAdapters = nullptr;
+    job = nullptr;
 }
 
 cInterface::~cInterface()
@@ -87,25 +176,52 @@ bool cInterface::close ()
     if (!ifcHandle)
         return true;
 
+    flushSendQueue();
+
     pcap_close(ifcHandle);
-    ifcHandle = NULL;
+    ifcHandle = nullptr;
 
     // free windows network adapter list
     if (winNetAdapters)
         free (winNetAdapters);
-    winNetAdapters = NULL;
+    winNetAdapters = nullptr;
+
 
     return true;
 }
 
-bool cInterface::sendPacket (const uint8_t* payload, size_t length) const
+bool cInterface::sendPacket (const uint8_t* payload, size_t length, const cTimeval& t)
 {
-    int ret = pcap_sendpacket (ifcHandle, (u_char*)payload, (int)length);
+    if (job) // queued send?
+    {
+        return job->addPacket (payload, length, t);
+    }
+    else
+    {
+        int ret = pcap_sendpacket (ifcHandle, (u_char*)payload, (int)length);
 
-    if (ret == -1)
-        Console::PrintError ("pcap error: %s\n", pcap_geterr (ifcHandle));
+        if (ret == -1)
+            Console::PrintError ("pcap error: %s\n", pcap_geterr (ifcHandle));
 
-    return ret == 0;
+        return ret == 0;
+    }
+}
+
+// packetCnt = 0 means endless loop
+bool cInterface::prepareSendQueue (int packetCnt, size_t totalBytes, bool synchronized)
+{
+    BUG_ON (!job); // we only support one job at a time
+    BUG_ON (ifcHandle);
+
+    job = new cJob (ifcHandle, packetCnt, totalBytes, synchronized);
+    return !!job;
+}
+
+bool cInterface::flushSendQueue (void)
+{
+    delete job;
+    job = nullptr;
+    return true;
 }
 
 bool cInterface::getMAC (cMacAddress& mac)
@@ -142,6 +258,11 @@ bool cInterface::getIPv4 (cIpAddress& ip)
          pUnicast = pUnicast->Next;
     }
     return false;
+}
+
+bool cInterface::isOpen () const
+{
+    return ifcHandle != NULL;
 }
 
 PIP_ADAPTER_ADDRESSES cInterface::getAdapterAddresses ()

src/os/win32/interface.hpp
@@ -21,26 +21,38 @@
 #define INTERFACE_H_
 
 #include <string>
+#include <thread>
 #include <cstdint>
 #include <cstddef>
 #include <iphlpapi.h>
 
 #include "ipaddress.hpp"
 #include "macaddress.hpp"
+#include "queue.hpp"
+#include "timeval.hpp"
 
+// forward declarations
 typedef struct pcap pcap_t;
-//typedef struct _IP_ADAPTER_ADDRESSES *PIP_ADAPTER_ADDRESSES;
+struct pcap_send_queue;
+class cJob;
 
 class cInterface
 {
+
 public:
     cInterface(const char* ifname);
     virtual ~cInterface();
     bool open ();
     bool close ();
-    bool sendPacket (const uint8_t* payload, size_t length) const;
+    bool sendPacket (const uint8_t* payload, size_t length, const cTimeval& t);
+    bool prepareSendQueue (int packetCnt, size_t totalBytes, bool synchronized);
+    bool flushSendQueue (void);
     bool getMAC (cMacAddress&);
     bool getIPv4 (cIpAddress&);
+    bool isOpen () const;
+
+    void operator()();
+
 
 private:
     PIP_ADAPTER_ADDRESSES getAdapterInfo ();
@@ -50,6 +62,7 @@ private:
     pcap_t *ifcHandle;
     PIP_ADAPTER_ADDRESSES winNetAdapters;
 
+    cJob* job;
 };
 
 #endif /* INTERFACE_H_ */

src/common/console.cpp
@@ -104,5 +104,7 @@ bool Console::print (out_level lvl, const char* format, va_list ap)
         return false;
 
      // we always print to stderr to be able to separate piped in/output from console prints
-     return vfprintf (stderr, format, ap) >= 0;
+    bool ret = vfprintf (stderr, format, ap) >= 0;
+    fflush (stderr);
+    return ret;
 }

src/os/win32/queue.hpp
@@ -1,0 +1,59 @@
+/*
+ * queue.hpp
+ *
+ *  Created on: 14.10.2020
+ *      Author: Andreas
+ */
+
+#ifndef SRC_OS_WIN32_QUEUE_HPP_
+#define SRC_OS_WIN32_QUEUE_HPP_
+
+
+#include <windows.h>
+
+// taken from https://stackoverflow.com/questions/3054701/is-there-a-set-of-win32-api-functions-to-manage-synchronized-queues
+
+template<class T, unsigned max = 256>
+class cQueue {
+    HANDLE space_avail; // at least one slot empty
+    HANDLE data_avail;  // at least one slot full
+    CRITICAL_SECTION mutex; // protect buffer, in_pos, out_pos
+
+    T buffer[max];
+    int in_pos, out_pos;
+public:
+    cQueue() : in_pos(0), out_pos(0) {
+        space_avail = CreateSemaphore(NULL, max, max, NULL);
+        data_avail = CreateSemaphore(NULL, 0, max, NULL);
+        InitializeCriticalSection(&mutex);
+    }
+
+    void push(T data) {
+        WaitForSingleObject(space_avail, INFINITE);
+        EnterCriticalSection(&mutex);
+        buffer[in_pos] = data;
+        in_pos = (in_pos + 1) % max;
+        LeaveCriticalSection(&mutex);
+        ReleaseSemaphore(data_avail, 1, NULL);
+    }
+
+    T pop() {
+        WaitForSingleObject(data_avail,INFINITE);
+        EnterCriticalSection(&mutex);
+        T retval = buffer[out_pos];
+        out_pos = (out_pos + 1) % max;
+        LeaveCriticalSection(&mutex);
+        ReleaseSemaphore(space_avail, 1, NULL);
+        return retval;
+    }
+
+    ~cQueue() {
+        DeleteCriticalSection(&mutex);
+        CloseHandle(data_avail);
+        CloseHandle(space_avail);
+    }
+};
+
+
+
+#endif /* SRC_OS_WIN32_QUEUE_HPP_ */

commit b4eda9c013cd3efaecba91e518ddea459b2128f1

fixed build problems introduced with bc625e474824815e3d53c77ea38a3aa09990c94b

src/CMakeLists.txt
@@ -677,6 +677,22 @@ add_test(NAME delay-23--ok COMMAND tcppump ${DEFAULT_PARAMS} -l2 -s ${TEST_FILE_
 add_test(NAME delay-23--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-23.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 add_test(NAME delay-24--ok COMMAND tcppump ${DEFAULT_PARAMS} -d1 -l2 -s ${TEST_FILE_PREFIX}/delay-24.pump WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 add_test(NAME delay-24--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-24.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-25-ok COMMAND tcppump ${DEFAULT_PARAMS} "1: arp(dip=10.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-25--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-25.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-26-ok COMMAND tcppump ${DEFAULT_PARAMS} -d2 "1: arp(dip=10.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-26--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-25.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-27-ok COMMAND tcppump ${DEFAULT_PARAMS} -tm "1: arp(dip=10.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-27--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-25.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-28-ok COMMAND tcppump ${DEFAULT_PARAMS} -tc "1: arp(dip=10.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-28--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-28.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-29-ok COMMAND tcppump ${DEFAULT_PARAMS} -l3 "2: arp(dip=10.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-29--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-29.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-30-ok COMMAND tcppump ${DEFAULT_PARAMS} -l3 "2: arp(dip=10.20.30.40)" "3: arp(dip=20.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-30--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-30.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-31-ok COMMAND tcppump ${DEFAULT_PARAMS} -l3 "2: arp(dip=10.20.30.40)" "arp(dip=20.20.30.40)" "+5: arp(dip=30.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-31--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-31.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-32-ok COMMAND tcppump ${DEFAULT_PARAMS} -d50 -l3 "2: arp(dip=10.20.30.40)" "arp(dip=20.20.30.40)" "+5: arp(dip=30.20.30.40)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+add_test(NAME delay-32--diff COMMAND cmake -E compare_files ${TEST_OUTPUT} ${TEST_FILE_PREFIX}/delay-32.pcap WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
 add_test(NAME online-1--ok COMMAND tcppump -i ${OUT_IFC} --dissect -vvvv "arp(dip=1.2.3.4)" WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 set_tests_properties(online-1--ok PROPERTIES PASS_REGULAR_EXPRESSION "Successfully sent 1 of 1 packets.")

src/os/linux/interface.cpp
@@ -34,6 +34,7 @@
 
 #include "bug.h"
 #include "console.hpp"
+#include "sleep.hpp"
 
 
 cInterface::cInterface(const char* ifname)
@@ -80,6 +81,8 @@ bool cInterface::open ()
     myMac.get(macAsString);
     Console::PrintDebug ("Successfully openend %s mac=%s\n", name.c_str(), macAsString.c_str());
 
+    lastSentPacket.clear();
+
     return (ifcHandle != -1);
 }
 
@@ -99,8 +102,17 @@ bool cInterface::close ()
     return true;
 }
 
-bool cInterface::sendPacket (const uint8_t* payload, size_t length) const
+bool cInterface::sendPacket (const uint8_t* payload, size_t length, const cTimeval& t)
 {
+    cTimeval sleepTime (t);
+    sleepTime.sub (lastSentPacket);
+
+    if (!sleepTime.isNull())
+    {
+        tcppump::Sleep (sleepTime);
+        lastSentPacket.add(sleepTime);
+    }
+
     struct sockaddr_ll device;
 
     memset (&device, 0, sizeof(device));

src/os/linux/interface.hpp
@@ -26,6 +26,7 @@
 
 #include "ipaddress.hpp"
 #include "macaddress.hpp"
+#include "timeval.hpp"
 
 
 class cInterface
@@ -35,8 +36,8 @@ public:
     virtual ~cInterface();
     bool open ();
     bool close ();
+    bool sendPacket (const uint8_t* payload, size_t length, const cTimeval& t);
     bool prepareSendQueue (int packetCnt, size_t totalBytes){return true;}
-    bool sendPacket (const uint8_t* payload, size_t length) const;
     bool flushSendQueue (void){return true;}
     bool getMAC (cMacAddress&);
     bool getIPv4 (cIpAddress&);
@@ -49,6 +50,7 @@ private:
     int ifIndex;
     cMacAddress myMac;
     cIpAddress myIP;
+    cTimeval lastSentPacket;
 };
 
 #endif /* INTERFACE_H_ */

src/tcppump.cpp
@@ -252,8 +252,11 @@ int cTcpPump::execute (int argc, char* argv[])
     double seconds = 0;
     if (!options.interactive)
     {
+        cTimeval currentTime;
         BUG_ON (packets.size() == delays.size());
+
         Console::PrintMoreVerbose ("Sending %d packets, each delayed by %" PRIu64 " usecs. ", packets.size(), activeDelay);
+
         bool endless = !options.repeat;
         if (!endless)
             Console::PrintMoreVerbose ("Repeating %d times.\n\n", options.repeat);
@@ -269,11 +272,12 @@ int cTcpPump::execute (int argc, char* argv[])
 
             for (auto & p : packets)
             {
+                currentTime.add (*t);
                 if (options.randSrcMac)
                     p.setRandomSrcMac();
                 if (options.randDstMac)
                     p.setRandomDestMac();
-                if (!sendPacket (ifc, *t, p))
+                if (!sendPacket (ifc, currentTime, p))
                     return -4;
                 t++;
                 sentBytes += p.getLength ();
@@ -304,9 +308,10 @@ bool cTcpPump::parsePackets (const cMacAddress& ownMac, const cIpAddress& ownIP,
     cTimeval timestamp, currtime;
     bool isAbsolute;
 
+    Console::PrintDebug ("Parsing %d packets (format='%s') ...\n", argc, options.raw ? "raw" : "tokens");
+
     for (int n = 0; n < argc; n++)
     {
-        Console::PrintDebug ("Parsing %d packets (format='%s') ...\n", argc, options.raw ? "raw" : "tokens");
         try
         {
             if (!options.raw)
@@ -325,7 +330,7 @@ bool cTcpPump::parsePackets (const cMacAddress& ownMac, const cIpAddress& ownIP,
                     }
                     else
                     {
-                        if (timestamp < currtime) // fixme Was tun wenn ein absoluter timestamp < currtime ist? delay = 0 oder Fehler melden?
+                        if (timestamp < currtime)  // FIXME What to do if timestamp < currtime? delay = 0 or parse exception?
                             BUG_ON ("fixme" == 0);
                         else
                         {
@@ -520,18 +525,13 @@ bool cTcpPump::parsePcapFiles (int pcapCnt, char* pcaps[])
 #endif
 
 
-bool cTcpPump::sendPacket (cInterface &ifc, const cTimeval &delay, const cEthernetPacket &p)
+bool cTcpPump::sendPacket (cInterface &ifc, const cTimeval &t, const cEthernetPacket &p)
 {
     triedToSendPackets++;
 
     if (!options.outpcap)
     {
-        if (!delay.isNull())
-        {
-            Console::PrintVerbose ("Waiting %u seconds\n", (unsigned)delay.s());
-            tcppump::Sleep (delay);
-        }
-        if(!ifc.sendPacket (p.get(), p.getLength()))
+        if(!ifc.sendPacket (p.get(), p.getLength(), t))
         {
             Console::PrintError ("Could not send packet.\n");
             return false;
@@ -540,7 +540,7 @@ bool cTcpPump::sendPacket (cInterface &ifc, const cTimeval &delay, const cEthern
 #if HAVE_PCAP
     else
     {
-        if (!outfile.write (delay, p.get(), (int)p.getLength(), false))
+        if (!outfile.write (t, p.get(), (int)p.getLength(), true))
             return false;
     }
 #endif

src/tcppump.hpp
@@ -70,7 +70,7 @@ private:
 #if HAVE_PCAP
     bool parsePcapFiles (int cnt, char* pcaps[]);
 #endif
-    bool sendPacket (cInterface &ifc, const cTimeval &delay, const cEthernetPacket &p);
+    bool sendPacket (cInterface &ifc, const cTimeval &t, const cEthernetPacket &p);
     bool interactiveMode (cInterface &ifc);
     void printParseError (const ParseException &e) const;
     void printParseError (const char* filename, const FileParseException &e) const;