ae329cab9ef481f912fbee8e0b473ad3419edb63
amartin755 <netnag@mailbox.org>
2021-06-28 08:57:49 +0200

    added missing vxlan source files

---
+++ src/packets/vxlanpacket.cpp
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * TCPPUMP <https://github.com/amartin755/tcppump>
+ * Copyright (C) 2012-2021 Andreas Martin (netnag@mailbox.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <cstring>
+
+#include "inet.h"
+#include "vxlanpacket.hpp"
+#include "bug.hpp"
+#include "inetchecksum.hpp"
+
+
+cVxlanPacket::cVxlanPacket ()
+{
+    std::memset (&header, 0, sizeof(header));
+    header.flags = htons(0x0800);
+}
+
+void cVxlanPacket::compile (const uint8_t* payload, size_t len)
+{
+    uint8_t* packet = new uint8_t[len + sizeof (header)];
+    std::memcpy(packet, &header, sizeof (header));
+    std::memcpy(packet + sizeof (header), payload, len);
+    cUdpPacket::compile (packet, len + sizeof (header));
+    delete[] packet;
+}
+
+void cVxlanPacket::setVni (uint32_t vni)
+{
+    vni = vni & 0x00ffffff;
+    header.vni = htonl(vni << 8);
+}
+
+
+#ifdef WITH_UNITTESTS
+#include "console.hpp"
+
+void cVxlanPacket::unitTest ()
+{
+    Console::PrintDebug("-- " __FILE__ " --\n");
+}
+#endif
---
+++ src/packets/vxlanpacket.hpp
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * TCPPUMP <https://github.com/amartin755/tcppump>
+ * Copyright (C) 2012-2021 Andreas Martin (netnag@mailbox.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef VXLAN_PACKET_H_
+#define VXLAN_PACKET_H_
+
+#include <cstdint>
+
+#include "udppacket.hpp"
+
+#pragma pack(1)
+typedef struct
+{
+    uint8_t  flags;
+    uint8_t resevered[3];
+    uint32_t vni; // only 24 bits
+
+}vxlan_header_t;
+#pragma pack()
+
+
+class cVxlanPacket : public cUdpPacket
+{
+public:
+    cVxlanPacket ();
+
+    void compile (const uint8_t* payload, size_t len);
+    void setVni (uint32_t vni);
+
+
+#ifdef WITH_UNITTESTS
+    static void unitTest ();
+#endif
+
+private:
+    vxlan_header_t header;
+};
+
+
+#endif /* VXLAN_PACKET_H_ */
