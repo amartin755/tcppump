d77974d7898865c766e2fa3e37a13bef8d27dc03
Andreas Martin <netnag@mailbox.org>
2024-12-26 19:14:57 +0100

    new test scripts for CLANG and UBSAN
    note: the CLANG build produces many warnings

---
+++ scripts/build-and-test-GCC-with-UBSAN.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+BUILDDIRNAME=build
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PROJROOT=$(realpath $SCRIPTPATH/..)
+
+
+# before we start, enforce successful sudo
+###############################################################################
+sudo -k
+echo "NOTE: need sudo for setting of raw-capabilities for tcppump binary"
+sudo true
+
+
+# clean configure and build
+###############################################################################
+rm -rf $PROJROOT/$BUILDDIRNAME
+cd $PROJROOT
+cmake -B $BUILDDIRNAME  -DWITH_UNITTESTS=ON -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DWITH_UBSAN=ON
+cmake --build build -j16
+
+
+# run all ctest cases
+###############################################################################
+
+# set raw capabilities
+sudo setcap cap_net_raw+eip $PROJROOT/bin/tcppump
+
+# execute ctest
+cd $PROJROOT/$BUILDDIRNAME
+ctest
