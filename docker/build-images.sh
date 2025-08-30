64d9990d731866d7d0f01d6edb64c003e21ef68f
Andreas Martin <netnag@mailbox.org>
2025-08-30 17:07:55 +0200

    removed german comments

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 set -e
 
-# Optional: Build-Argumente für UID/GID setzen
+# set UID/GID
 UID_ARG=$(id -u)
 GID_ARG=$(id -g)
 
-# Alle Dockerfile.* im aktuellen Verzeichnis durchsuchen
+# iterate over all Dockerfile.* in current directory
 for dockerfile in Dockerfile.*; do
-    # Prüfen, ob Dateien existieren
+
     [ -e "$dockerfile" ] || continue
 
-    # Tag aus Dateiname ableiten, z.B. Dockerfile.debian -> cpp-env:debian
     tag="tcppump:${dockerfile#Dockerfile.}"
 
-    echo "======================================"
-    echo "Building $dockerfile with tag $tag"
-    echo "======================================"
+    echo "==============================================================="
+    echo " Building $dockerfile with tag $tag"
+    echo "==============================================================="
 
     docker build --build-arg UID=$UID_ARG --build-arg GID=$GID_ARG -f "$dockerfile" -t "$tag" .
 #    docker build  -f "$dockerfile" -t "$tag" .
 done
 
-echo "Alle Dockerfiles erfolgreich gebaut!"
+echo "all images successfully built"
