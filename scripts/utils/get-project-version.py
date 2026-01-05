1e49f18390054b6dd0cb2d39c7c82c1322dec0fc
Andreas Martin <netnag@mailbox.org>
2026-01-05 13:21:44 +0100

    scripts: add SPDX license header and copyright notice to all script files

--- scripts/utils/get-project-version.py
+++ scripts/utils/get-project-version.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-3.0-only
+###############################################################################
+#
+# TCPPUMP <https://github.com/amartin755/tcppump>
+# Copyright (C) 2012-2026 Andreas Martin (netnag@mailbox.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
 import re
 import sys
