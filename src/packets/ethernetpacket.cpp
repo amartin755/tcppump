cf48b4f9fe2c1f70651005d4956eab9a6898593d
Andreas Martin <netnag@mailbox.org>
2026-07-26 13:09:27 +0200

    keep temporary copy of old ethernet packet compiler, to not brake the build

---
+++ src/packets/ethernetpacket.cpp
@@ -0,0 +1,589 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * TCPPUMP <https://github.com/amartin755/tcppump>
+ * Copyright (C) 2012-2025 Andreas Martin (netnag@mailbox.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// FIXME Delete this file before the branch is merged to devel
+#include <cstring>
+
+#include "ethernetpacket.hpp"
+#include "settings.hpp"
+#include "bug.hpp"
+
+
+
+cEthernetPacket::cEthernetPacket ()
+: cEthernetPacket (cSettings::get().getMyMTU() + sizeof (mac_header_t) + 4)
+{
+}
+
+
+cEthernetPacket::cEthernetPacket (size_t maxLength)
+{
+    BUG_ON (maxLength < sizeof (mac_header_t));
+
+    /*
+     * note: We use an array of uint32_t here to force 32bit aligment of our packet data.
+     * Yes, that's paranoid. I know that 'new' always aligns to __STDCPP_DEFAULT_NEW_ALIGNMENT__, which is
+     * typically 8 or 16 bytes, but you never know ;-)
+     */
+    data = new uint32_t[(maxLength + sizeof (uint32_t) - 1) / sizeof (uint32_t)];
+
+    packet          = (uint8_t*)data;
+    packetMaxLength = maxLength;
+    std::memset (packet, 0, sizeof (mac_header_t)); // initialize at least the ethernet header to zero
+    reset ();
+}
+
+// move constructor
+cEthernetPacket::cEthernetPacket (cEthernetPacket&& other)
+{
+    data             = other.data;
+    packet           = other.packet;
+    packetMaxLength  = other.packetMaxLength;
+    pPayload         = other.pPayload;
+    pEthertypeLength = other.pEthertypeLength;
+    payloadLength    = other.payloadLength;
+    llcHeaderLength  = other.llcHeaderLength;
+    hasDMAC          = other.hasDMAC;
+
+    other.data             = nullptr;
+    other.packet           = nullptr;
+    other.packetMaxLength  = 0;
+    other.pPayload         = nullptr;
+    other.pEthertypeLength = nullptr;
+    other.payloadLength    = 0;
+    other.llcHeaderLength  = 0;
+}
+
+// copy constructor
+cEthernetPacket::cEthernetPacket (const cEthernetPacket& obj) : cEthernetPacket (obj.packetMaxLength)
+{
+    BUG_ON (packetMaxLength != obj.packetMaxLength);
+
+    payloadLength    = obj.payloadLength;
+    llcHeaderLength  = obj.llcHeaderLength;
+    hasDMAC          = obj.hasDMAC;
+
+    // copy packet data
+    std::memcpy (packet, obj.packet, obj.getLength());
+
+    // recalculate offsets
+    pPayload         = packet + (obj.pPayload - obj.packet);
+    pEthertypeLength = (uint16_t*)(packet + ((uint8_t*)obj.pEthertypeLength - obj.packet));
+}
+
+
+cEthernetPacket::~cEthernetPacket ()
+{
+    delete[] data;
+}
+
+
+cEthernetPacket& cEthernetPacket::operator=(cEthernetPacket&& other)
+{
+    if (this != &other)
+    {
+        delete[] data;
+
+        data             = other.data;
+        packet           = other.packet;
+        packetMaxLength  = other.packetMaxLength;
+        pPayload         = other.pPayload;
+        pEthertypeLength = other.pEthertypeLength;
+        payloadLength    = other.payloadLength;
+        llcHeaderLength  = other.llcHeaderLength;
+        hasDMAC          = other.hasDMAC;
+
+        other.data             = nullptr;
+        other.packet           = nullptr;
+        other.packetMaxLength  = 0;
+        other.pPayload         = nullptr;
+        other.pEthertypeLength = nullptr;
+        other.payloadLength    = 0;
+        other.llcHeaderLength  = 0;
+    }
+
+    return *this;
+}
+
+
+void cEthernetPacket::reset ()
+{
+    pPayload          = packet;
+    pPayload         += sizeof (mac_header_t);
+    pEthertypeLength  = (uint16_t*)(&((mac_header_t*)packet)->ethertypeLength);
+    payloadLength     = 0;
+    llcHeaderLength   = 0;
+    hasDMAC           = false;
+    *pEthertypeLength = 0;
+}
+
+
+void cEthernetPacket::updatePosition (size_t len)
+{
+    pPayload += len;
+    pEthertypeLength = (uint16_t*)((uint8_t*)pEthertypeLength + len);
+}
+
+
+void cEthernetPacket::setTypeLength (uint16_t ethertypeLength)
+{
+    *pEthertypeLength = htons (ethertypeLength);
+}
+
+
+void cEthernetPacket::setLength ()
+{
+    setTypeLength (uint16_t(payloadLength + llcHeaderLength));
+}
+
+
+void cEthernetPacket::setMacHeader (const cMacAddress& src, const cMacAddress& dest)
+{
+    setSrcMac (src);
+    setDestMac (dest);
+}
+
+
+void cEthernetPacket::setDestMac (const cMacAddress& dest)
+{
+    // mac header contains source and destination mac and is always at the begin of the packet
+    mac_header_t* header = (mac_header_t*)packet;
+    std::memcpy(&header->dest, dest.get(), dest.size());
+    hasDMAC = true;
+}
+
+
+void cEthernetPacket::getDestMac (cMacAddress& dest) const
+{
+    mac_header_t* header = (mac_header_t*)packet;
+    dest.set(&header->dest, sizeof (header->dest));
+}
+
+
+void cEthernetPacket::setSrcMac (const cMacAddress& src)
+{
+    // mac header contains source and destination mac and is always at the begin of the packet
+    mac_header_t* header = (mac_header_t*)packet;
+    std::memcpy(&header->src, src.get(), src.size());
+}
+
+
+void cEthernetPacket::getSrcMac (cMacAddress& src) const
+{
+    mac_header_t* header = (mac_header_t*)packet;
+    src.set(&header->src, sizeof (header->src));
+}
+
+
+void cEthernetPacket::addLlcHeader (uint8_t dsap, uint8_t ssap, uint16_t control)
+{
+    // size of the control word can either be 8 or 16 bits (depends on its content)
+    llcHeaderLength = (control & 0x0003) == 3 ?
+            sizeof (llc_t) - 1 : sizeof (llc_t);
+
+    checkPacketLength (llcHeaderLength);
+
+    // if there is already payload, we move it
+    if (payloadLength)
+    {
+        std::memmove (pPayload + llcHeaderLength, pPayload, payloadLength);
+    }
+
+    llc_t* llc = (llc_t*)pPayload;
+
+    llc->dsap = dsap;
+    llc->ssap = ssap;
+
+    if ((control & 0x0003) == 3)
+    {
+        llc->control.c8 = (uint8_t)control;
+    }
+    else
+    {
+        llc->control.c16 = htons (control);
+    }
+
+    pPayload += llcHeaderLength;
+    setLength ();
+}
+
+
+void cEthernetPacket::addSnapHeader (uint32_t oui, uint16_t protocol)
+{
+    addLlcHeader (0xaa, 0xaa, 3);
+
+    checkPacketLength (sizeof (snap_t));
+
+    // if there is already payload, we move it
+    if (payloadLength)
+    {
+        std::memmove (pPayload + sizeof (snap_t), pPayload, payloadLength);
+    }
+
+    llcHeaderLength += sizeof (snap_t);
+    oui = htonl (oui);
+
+    snap_t* snap   = (snap_t*)pPayload;
+    snap->oui.a    = uint8_t((oui >>  8) & 0x000000ff);
+    snap->oui.b    = uint8_t((oui >> 16) & 0x000000ff);
+    snap->oui.c    = uint8_t((oui >> 24) & 0x000000ff);
+    snap->protocol = htons (protocol);
+
+    pPayload += sizeof (snap_t);
+    setLength ();
+}
+
+
+void cEthernetPacket::addVlanTag (bool isCTag, uint16_t id, uint16_t prio, uint16_t dei)
+{
+    checkPacketLength (sizeof (vlan_t));
+
+    vlan_t* tag = (vlan_t*)pEthertypeLength;
+
+    std::memmove ((uint8_t*)pEthertypeLength + sizeof (vlan_t), pEthertypeLength, 2 + payloadLength);
+    isCTag ? tag->setCTag (id, prio, dei) : tag->setSTag (id, prio, dei);
+    updatePosition (sizeof (vlan_t));
+}
+
+
+void cEthernetPacket::setPayload (const uint8_t* payload, size_t len)
+{
+    payloadLength = 0;
+    checkPacketLength (len);
+    std::memcpy (pPayload, payload, len);
+    payloadLength = len;
+}
+
+
+void cEthernetPacket::appendPayload (const uint8_t* payload, size_t len)
+{
+    checkPacketLength (len);
+    uint8_t* p = pPayload + payloadLength;
+    std::memcpy (p, payload, len);
+    payloadLength += len;
+}
+
+
+void cEthernetPacket::setRaw (const uint8_t* payload, size_t len)
+{
+    reset ();
+    if (len > packetMaxLength)
+        throw FormatException (exParRange, NULL);
+    std::memcpy (packet, payload, len);
+    payloadLength = len - sizeof (mac_header_t);
+    hasDMAC       = true;
+}
+
+
+const uint8_t* cEthernetPacket::get () const
+{
+    return packet;
+}
+
+
+void cEthernetPacket::updatePayloadAt (unsigned offset, const void* payload, size_t len)
+{
+    if ((offset + len) > payloadLength)
+        throw FormatException (exParRange, NULL);
+
+    std::memcpy (&(pPayload[offset]), payload, len);
+}
+
+
+#ifdef WITH_UNITTESTS
+#include "console.hpp"
+
+void cEthernetPacket::unitTest ()
+{
+    Console::PrintDebug("-- " __FILE__ " --\n");
+
+    cMacAddress src("12:34:56:78:9a:bc");
+    cMacAddress dst("11:22:33:44:55:66");
+
+
+    try
+    {
+        cEthernetPacket obj(MAX_DOUBLE_TAGGED_PACKET + 1);
+        memset (obj.packet, 0xcc, MAX_DOUBLE_TAGGED_PACKET + 1);
+        obj.clear();
+
+        obj.setMacHeader(src, dst);
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x00\x00\xcc\xcc", 16));
+        BUG_IF_NOT (obj.getLength() == 14);
+        obj.setLength ();
+        BUG_IF_NOT (obj.getLength() == 14);
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x00\x00\xcc\xcc", 16));
+        obj.setTypeLength (0x1234);
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x12\x34\xcc\xcc", 16));
+        BUG_IF_NOT (obj.getLength() == 14);
+        obj.addVlanTag(false, 12, 7, 0);
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x88\xa8\xe0\x0c\x12\x34\xcc\xcc", 20));
+        BUG_IF_NOT (obj.getLength() == 18);
+        obj.setPayload ((uint8_t*)"\xaa\xbb\xcc\xdd\xee\xff\x0a\x0b\x0c\x0d\x0e\x0f", 12);
+        BUG_IF_NOT (obj.getLength() == 30);
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x88\xa8\xe0\x0c\x12\x34\xaa\xbb\xcc\xdd\xee\xff\x0a\x0b\x0c\x0d\x0e\x0f\xcc\xcc", 32));
+        obj.addVlanTag(true, 12, 7, 0);
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x88\xa8\xe0\x0c\x81\x00\xe0\x0c\x12\x34\xaa\xbb\xcc\xdd\xee\xff\x0a\x0b\x0c\x0d\x0e\x0f\xcc\xcc", 36));
+        BUG_IF_NOT (obj.getLength() == 34);
+        obj.setLength();
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x88\xa8\xe0\x0c\x81\x00\xe0\x0c\x00\x0c\xaa\xbb\xcc\xdd\xee\xff\x0a\x0b\x0c\x0d\x0e\x0f\xcc\xcc", 36));
+        obj.addLlcHeader(0x10, 0x20, 3);
+        BUG_IF_NOT (obj.getLength() == 37);
+
+        {
+            // test copy constructor
+            cEthernetPacket cpy(obj);
+            BUG_IF_NOT (obj.data != cpy.data);
+            BUG_IF_NOT (obj.packet != cpy.packet);
+            BUG_IF_NOT (obj.pPayload != cpy.pPayload);
+            BUG_IF_NOT (obj.pEthertypeLength != cpy.pEthertypeLength);
+            BUG_IF_NOT (obj.packetMaxLength == cpy.packetMaxLength);
+            BUG_IF_NOT (obj.payloadLength == cpy.payloadLength);
+            BUG_IF_NOT (obj.llcHeaderLength == cpy.llcHeaderLength);
+            BUG_IF_NOT (*obj.data == *cpy.data);
+            BUG_IF_NOT (*obj.packet == *cpy.packet);
+            BUG_IF_NOT (*obj.pPayload == *cpy.pPayload);
+            BUG_IF_NOT (*obj.pEthertypeLength == *cpy.pEthertypeLength);
+            BUG_IF_NOT (!memcmp (obj.packet, cpy.packet, (obj.pPayload + obj.payloadLength) - obj.packet));
+        }
+
+        memset (obj.packet, 0xcc, MAX_DOUBLE_TAGGED_PACKET + 1);
+        obj.reset();
+        obj.setMacHeader(src, dst);
+        obj.addSnapHeader(0x00808182, 0x9876);
+        BUG_IF_NOT (!memcmp (obj.packet, "\x11\x22\x33\x44\x55\x66\x12\x34\x56\x78\x9a\xbc\x00\x08\xaa\xaa\x03\x80\x81\x82\x98\x76\xcc\xcc", 24));
+        BUG_IF_NOT (obj.getLength() == 22);
+
+        {
+            // test copy constructor
+            cEthernetPacket cpy(obj);
+            BUG_IF_NOT (obj.data != cpy.data);
+            BUG_IF_NOT (obj.packet != cpy.packet);
+            BUG_IF_NOT (obj.pPayload != cpy.pPayload);
+            BUG_IF_NOT (obj.pEthertypeLength != cpy.pEthertypeLength);
+            BUG_IF_NOT (obj.packetMaxLength == cpy.packetMaxLength);
+            BUG_IF_NOT (obj.payloadLength == cpy.payloadLength);
+            BUG_IF_NOT (obj.llcHeaderLength == cpy.llcHeaderLength);
+            BUG_IF_NOT (*obj.data == *cpy.data);
+            BUG_IF_NOT (*obj.packet == *cpy.packet);
+            BUG_IF_NOT (*obj.pEthertypeLength == *cpy.pEthertypeLength);
+            BUG_IF_NOT (!memcmp (obj.packet, cpy.packet, obj.getLength()));
+        }
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+
+    bool catched = false;
+    try
+    {
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(vlan_t));
+        obj.addVlanTag(false, 12, 7, 0);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(vlan_t)-1);
+        obj.addVlanTag(false, 12, 7, 0);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+
+    try
+    {
+        cEthernetPacket obj(sizeof (mac_header_t) + 2*sizeof(vlan_t));
+        obj.addVlanTag(false, 12, 7, 0);
+        obj.addVlanTag(true, 12, 7, 0);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        cEthernetPacket obj(sizeof (mac_header_t) + 2*sizeof(vlan_t)-1);
+        obj.addVlanTag(false, 12, 7, 0);
+        obj.addVlanTag(true, 12, 7, 0);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+
+    try
+    {
+        cEthernetPacket obj(sizeof (mac_header_t));
+        obj.setRaw((uint8_t*)"\x12\x34\x56\x78\x90\x12\x34\x56\x78\x90\xaa\xbb\xcc\xdd", 14);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        cEthernetPacket obj(sizeof (mac_header_t));
+        obj.setRaw((uint8_t* )"\x12\x34\x56\x78\x90\x12\x34\x56\x78\x90\xaa\xbb\xcc\xdd\xee", 15);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+
+    try
+    {
+        cEthernetPacket obj(sizeof (mac_header_t) + 1);
+        obj.setPayload ((uint8_t* )"\xaa", 1);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        cEthernetPacket obj(sizeof (mac_header_t) + 1);
+        obj.setPayload ((uint8_t* )"\xaa\xbb", 2);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+
+    try
+    {
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(llc_t));
+        obj.addLlcHeader(12, 34, 0);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(llc_t)-1);
+        obj.addLlcHeader(12, 34, 0);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+
+    try
+    {
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(llc_t) - 1);
+        obj.addLlcHeader(12, 34, 3);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(llc_t) - 1 - 1);
+        obj.addLlcHeader(12, 34, 3);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+
+    try
+    {
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(llc_t) + sizeof(snap_t) - 1);
+        obj.addSnapHeader(0x123456, 1234);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        cEthernetPacket obj(sizeof (mac_header_t) + sizeof(llc_t) + sizeof(snap_t) - 1 - 1);
+        obj.addSnapHeader(0x123456, 1234);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+
+    try
+    {
+        uint8_t payload[(MAX_DOUBLE_TAGGED_PACKET - 30)];
+        memset (payload, 0, sizeof (payload));
+        cEthernetPacket obj(MAX_DOUBLE_TAGGED_PACKET+1);
+        memset (obj.packet, 0xcc, MAX_DOUBLE_TAGGED_PACKET+1);
+        obj.reset();
+        obj.addVlanTag(false, 12, 7, 0);
+        BUG_IF_NOT (obj.packet[18] == 0xcc);
+        obj.addVlanTag(true, 12, 7, 0);
+        BUG_IF_NOT (obj.packet[22] == 0xcc);
+        obj.addSnapHeader(0x123456, 1234);
+        BUG_IF_NOT (obj.packet[30] == 0xcc);
+        obj.setPayload (payload, sizeof (payload));
+        BUG_IF_NOT (obj.packet[30] == 0);
+        BUG_IF_NOT (obj.packet[MAX_DOUBLE_TAGGED_PACKET-1] == 0);
+        BUG_IF_NOT (obj.packet[MAX_DOUBLE_TAGGED_PACKET] == 0xcc);
+    }
+    catch (FormatException& )
+    {
+        BUG_IF_NOT (0);
+    }
+    try
+    {
+        catched = false;
+        uint8_t payload[(MAX_DOUBLE_TAGGED_PACKET - 29)];
+        memset (payload, 0, sizeof (payload));
+        cEthernetPacket obj(MAX_DOUBLE_TAGGED_PACKET);
+        memset (obj.packet, 0xcc, MAX_DOUBLE_TAGGED_PACKET);
+        obj.reset();
+        obj.addVlanTag(false, 12, 7, 0);
+        BUG_IF_NOT (obj.packet[18] == 0xcc);
+        obj.addVlanTag(true, 12, 7, 0);
+        BUG_IF_NOT (obj.packet[22] == 0xcc);
+        obj.addSnapHeader(0x123456, 1234);
+        BUG_IF_NOT (obj.packet[30] == 0xcc);
+        obj.setPayload (payload, sizeof (payload));
+        BUG_IF_NOT (obj.packet[30] == 0);
+        BUG_IF_NOT (obj.packet[MAX_DOUBLE_TAGGED_PACKET-1] == 0);
+    }
+    catch (FormatException& )
+    {
+        catched = true;
+    }
+    BUG_IF_NOT (catched);
+}
+#endif
---
+++ src/packets/ethernetpacket.hpp
@@ -0,0 +1,218 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * TCPPUMP <https://github.com/amartin755/tcppump>
+ * Copyright (C) 2012-2025 Andreas Martin (netnag@mailbox.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// FIXME Delete this file before the branch is merged to devel
+#ifndef ETHERNET_PACKET_H_
+#define ETHERNET_PACKET_H_
+
+#include <cstdint>
+#include <cstddef>    // size_t
+
+#include "bug.hpp"
+#include "formatexception.hpp"
+#include "inet.h"
+#include "macaddress.hpp"
+#include "linkable.hpp"
+
+
+class cEthernetPacket : public cLinkable
+{
+public:
+    cEthernetPacket ();
+    cEthernetPacket (size_t maxLength);
+    cEthernetPacket (const cEthernetPacket& obj); // copy constructor
+    virtual ~cEthernetPacket ();
+    cEthernetPacket (cEthernetPacket&& other);
+    cEthernetPacket& operator=(cEthernetPacket&& other);
+    void operator=(const cEthernetPacket&) = delete;       // no copy-assignment operator
+
+    void setRandomSrcMac (bool unicast = true, bool multicast = false)
+    {
+        setSrcMac (cMacAddress (unicast, multicast));
+    }
+    void setRandomDestMac (bool unicast = true, bool multicast = false)
+    {
+        setDestMac (cMacAddress (unicast, multicast));
+    }
+    void setSrcMac (const cMacAddress& src);
+    void getSrcMac (cMacAddress& src) const;
+    void setDestMac (const cMacAddress& dest);
+    void getDestMac (cMacAddress& dest) const;
+    void setMacHeader (const cMacAddress& src, const cMacAddress& dest);
+    void addLlcHeader (uint8_t dsap, uint8_t ssap, uint16_t control);
+    void addSnapHeader (uint32_t oui, uint16_t protocol);
+    void addVlanTag (bool isCTag, uint16_t id, uint16_t prio, uint16_t dei);
+    void setTypeLength (uint16_t ethertypeLenth);
+    void setLength ();
+    void setPayload (const uint8_t* payload, size_t len);
+    void appendPayload (const uint8_t* payload, size_t len);
+    void setRaw (const uint8_t* payload, size_t len);
+    const uint8_t* get () const;
+    inline size_t getLength () const {return pPayload - packet + payloadLength;}
+    inline void clear () {reset ();};
+    inline bool hasLlcHeader () const {return llcHeaderLength != 0;}
+    inline bool hasPayload () const {return payloadLength != 0;}
+    inline bool hasDestMac () const {return hasDMAC;}
+    inline const uint8_t * getPayload () const {return pPayload;}
+    inline uint8_t getPayloadAt8 (unsigned offset) const
+    {
+        if (offset > payloadLength)
+            throw FormatException (exParRange, NULL);
+
+        return pPayload[offset];
+    }
+    inline uint16_t getPayloadAt16 (unsigned offset) const // note: offset is a byte offset!!!
+    {
+        if (offset > payloadLength)
+            throw FormatException (exParRange, NULL);
+
+        return ((uint16_t*)pPayload)[offset/2];
+    }
+    inline size_t getPayloadLength () const {return payloadLength;}
+    inline uint16_t getTypeLength () const {return ntohs(*pEthertypeLength);}
+    void updatePayloadAt (unsigned offset, const void* payload, size_t len);
+
+    static const size_t   MAX_ETHERNET_PAYLOAD     = 1500;
+    static const size_t   MAX_PACKET               = 6+6+2+MAX_ETHERNET_PAYLOAD;
+    static const size_t   MAX_TAGGED_PACKET        = MAX_PACKET + 4;
+    static const size_t   MAX_DOUBLE_TAGGED_PACKET = MAX_TAGGED_PACKET + 4;
+
+#ifdef WITH_UNITTESTS
+    static void unitTest ();
+#endif
+
+protected:
+    inline size_t getMtu () const {return pPayload - packet + packetMaxLength;}
+
+private:
+    void reset ();
+    void updatePosition (size_t len);
+    inline void checkPacketLength (size_t addedBytes) const
+    {
+        if ((getLength () + addedBytes) > packetMaxLength)
+            throw FormatException (exParRange, NULL);
+    }
+
+    const uint32_t* data;       // holds the packet data; do never access directly; use packet instead!
+    uint8_t*  packet;            // always points to packet begin
+    size_t    packetMaxLength;
+    uint8_t*  pPayload;         // points at begin of payload (will be moved in case of tagging)
+    uint16_t* pEthertypeLength; // points at ethertype/length field (will be moved in case of tagging)
+    size_t    payloadLength;
+    size_t    llcHeaderLength;
+    bool      hasDMAC;
+};
+
+enum ethertypes_t : uint16_t
+{
+    ETHERTYPE_IPV4  = 0x0800,
+    ETHERTYPE_ARP   = 0x0806,
+    ETHERTYPE_CVLAN = 0x8100,
+    ETHERTYPE_IPV6  = 0x86DD,
+    ETHERTYPE_SVLAN = 0x88a8,
+    ETHERTYPE_PN    = 0x8892,
+    ETHERTYPE_LLDP  = 0x88CC
+};
+
+#pragma pack(1)
+
+struct mac_header_t
+{
+    cMacAddress::mac_t  dest;
+    cMacAddress::mac_t  src;
+    uint16_t ethertypeLength;
+};
+
+struct vlan_t
+{
+    uint16_t tpid;
+    uint16_t tci;  // tag control information | prio (3 bit) | CFI/DEI (1 bit) | vlan id (12 bit)
+
+public:
+    void setCTag (uint16_t id, uint16_t prio = 0, uint16_t dei = 0)
+    {
+        tpid = htons (ETHERTYPE_CVLAN);
+        setTci (id, prio, dei);
+    }
+    void setSTag (uint16_t id, uint16_t prio = 0, uint16_t dei = 0)
+    {
+        tpid = htons (ETHERTYPE_SVLAN);
+        setTci (id, prio, dei);
+    }
+    unsigned getId () const
+    {
+        return unsigned (ntohs (tci) & 0x03ff);
+    }
+    unsigned getPrio () const
+    {
+        return unsigned ((ntohs (tci) >> 13) & 0x0007);
+    }
+    unsigned getDEI () const
+    {
+        return unsigned ((ntohs (tci) >> 12) & 0x0001);
+    }
+    bool isVlan () const
+    {
+        uint16_t type = ntohs (tpid);
+        return type == ETHERTYPE_CVLAN || type == ETHERTYPE_SVLAN;
+    }
+    bool isCVlan () const
+    {
+        return ntohs (tpid) == ETHERTYPE_CVLAN;
+    }
+    bool isPVlan () const
+    {
+        return ntohs (tpid) == ETHERTYPE_SVLAN;
+    }
+
+
+private:
+    void setTci (uint16_t vid, uint16_t prio, uint16_t dei)
+    {
+        tci = htons ((vid & 0x0FFF)| ((dei & 1) << 12) | ((prio & 7) << 13));
+    }
+
+};
+
+struct llc_t
+{
+    uint8_t  dsap;
+    uint8_t  ssap;
+    union
+    {
+        uint16_t c16;
+        uint8_t  c8;
+    }control;
+};
+
+struct oui_t
+{
+    uint8_t a;
+    uint8_t b;
+    uint8_t c;
+};
+
+struct snap_t
+{
+    oui_t    oui;
+    uint16_t protocol;
+};
+
+#pragma pack()
+
+#endif /* ETHERNET_PACKET_H_ */
